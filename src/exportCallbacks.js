6bd8a0d39ee7a79aa62b12c9a701ff34512ac38d
Jeff Handley <jeff.handley@concur.com>
2017-05-31 09:03:10 -0400

    Add JSDoc comments for exportCallbacks

--- src/exportCallbacks.js
+++ src/exportCallbacks.js
@@ -1,3 +1,8 @@
+/**
+ * Export callbacks' subscribe functions
+ * @param {object} callbacks Each property is a callback from createCallback() with a subscribe() function
+ * @return {object} An object with properties for each callback exported, each being the subscribe() function from the callback
+ */
 export default function exportCallbacks(callbacks) {
     const exported = {};
 
